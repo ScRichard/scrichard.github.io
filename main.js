409f58169ed2a869087a40b7f759587485730b77
Richard Schmidt <104382590+ScRichard@users.noreply.github.com>
2024-10-09 15:27:47 +0200

    Commit
    New special features

--- main.js
+++ main.js
@@ -42,29 +42,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     move();
 });
-const isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
+function isMobile() {
+    return (( window.innerWidth <= 800 ) || ( window.innerHeight <= 600 ) )
 };
 
 
-if (!isMobile.any()){
+if (!isMobile()){
     setInterval(() => {
         if(hover) {
             var offset = item.getBoundingClientRect();
